8dac6c1f125a74692c3f67b4cfdf118271a2cffc
papajo <joshipv2@gmail.com>
2018-05-25 19:11:01 -0400

    test the hashBlock Step using SHA-256 encryption method

--- dev/test.js
+++ dev/test.js
@@ -1,33 +1,31 @@
+// testing testing
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 const previousBlockHash = 'LKJASLJDLA87878';
 const currentBlockData = [
 	{
-		amount: 10,
-		sender: 'KJSLKJAD098098',
-		recipient: 'OIUYLKJASD98798'
-	},
-	{
-		amount: 30,
-		sender: 'DFDKJSLKJAD098098',
-		recipient: '124SDFGLKJASD98798'
+		amount: 100,
+		sender: 'KJSLKJAD098ABC',
+		recipient: 'OIUYLKJASD98XYZ'
 	},
 	{
 		amount: 200,
-		sender: 'SSWEKJSLKJAD098098',
-		recipient: 'QWDS23LKJASD98798'
+		sender: 'DFDKJSLKJAD098ABC',
+		recipient: '124SDFGLKJASD98QWR'
+	},
+	{
+		amount: 300,
+		sender: 'SSWEKJSLKJAD098ABC',
+		recipient: 'QWDS23LKJASD98QWR'
 	},
 ];
-const nonce = 100;
+const nonce = 500;
 
-
-
-
-
+//test hashBlock Step using SHA-256 encryption method
 
 console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
 
-
+//The following work fine
 //bitcoin.createNewBlock(1234, 'OIIHASDO23234243', 'UIHKH657532');
 //bitcoin.createNewBlock(1235, '323HASDO23234243', '121UIHKH657532');
 //bitcoin.createNewBlock(1236, '2324HASDO23234243', '212UIHKH657532');
