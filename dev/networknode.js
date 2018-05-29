2d3c7dae0708f08cb589c944e569fb5d57aee2c0
papajo <joshipv2@gmail.com>
2018-05-29 17:47:08 -0400

    create distributed network node to run multiple instances

---
+++ dev/networknode.js
@@ -0,0 +1,52 @@
+//API 
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+const nodeAddress = uuid().split('-').join('');
+const port = process.argv[2];
+const kalpacoin = new Blockchain();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//app.get('/', function(req, res) {
+//	res.send('App started...')
+//})
+
+//fetch entire blockchain
+app.get('/blockchain', function(req, res) {
+	res.send(kalpacoin);
+}); 
+
+//create a new transaction
+app.post('/transaction', function(req, res) {
+	const blockIndex = kalpacoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+	res.json({ note: `transaction will be added in block ${blockIndex}.` });
+
+});
+
+//mine a new block
+app.get('/mine', function(req, res) {
+	const lastBlock = kalpacoin.getLastBlock();
+	const previousBlockHash = lastBlock['hash'];
+	const currentBlockData = {
+		transactions: kalpacoin.pendingTransactions,
+		index: lastBlock['index'] + 1
+	}
+	const nonce = kalpacoin.proofOfWork(previousBlockHash, currentBlockData);
+	const blockHash = kalpacoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+	//mining reward 12.5 as of 2018
+	kalpacoin.createNewTransaction(12.5, "00", nodeAddress);
+	const newBlock = kalpacoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+	res.json({ note: "New block mined successfully",
+			   block: newBlock	
+	})
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${ port }...`);
+});
